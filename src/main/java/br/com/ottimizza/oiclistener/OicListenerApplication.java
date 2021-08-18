f033c1ecda006cdd0950ee384650bdfd376ecaf7
Daniel Steil <dani.steil@hotmail.com>
2021-08-18 11:30:10 -0300

    criando projeto

---
+++ src/main/java/br/com/ottimizza/oiclistener/OicListenerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ottimizza.oiclistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OicListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OicListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ottimizza/oiclistener/OicListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ottimizza.oiclistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OicListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
